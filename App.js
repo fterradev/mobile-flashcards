ec79dadc886a8e20825afd7688dc030c6a1ffe2c
Fernando Terra <nandobrt@gmail.com>
2018-01-27 18:08:02 -0200

    feat: Add Tabs

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import AddDeck from './components/AddDeck';
+import Decks from './components/Decks';
+import { TabNavigator } from 'react-navigation';
+import { Constants } from 'expo';
+
+function AppStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks'
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck'
+    }
+  }
+});
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <AddDeck />
+      <View style={{ flex: 1 }}>
+        <AppStatusBar backgroundColor="purple" barStyle="light-content" />
+        <Tabs />
       </View>
     );
   }
---
+++ components/Decks.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import {
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+
+export default class Decks extends Component {
+  render() {
+    return null;
+  }
+}
